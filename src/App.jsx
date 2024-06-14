6783a6448128250fc11441a0c1821fc30cc8f2b2
Raja Sodani <148634774+RajaSodani@users.noreply.github.com>
2024-06-14 08:30:09 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
 				}
 			>
 				<Route path="" element={<Navigate to="todos" />} />
-{/* 				<Route path="" element={} /> */}
+				<Route path="" element={<Todos/>} />
 				<Route path="share" element={<Share />} />
 			</Route>,
 		),
